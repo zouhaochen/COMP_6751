bfcfdf0abeff84103d020635b2791c08907f74bf
NormalZHC <zouhaochenluke@gmail.com>
2021-09-24 14:06:04 -0400

    create PreProcess.py

--- Assignment1/EarleyParsing.py
+++
@@ -1,3 +0,0 @@
-import nltk
-
-nltk.parse.chart.demo(2, print_times=False, trace=1, sent='John saw a dog', numparses=1)
--- Assignment1/ReutersTest.py
+++
@@ -1,6 +0,0 @@
-from nltk.corpus import reuters
-
-files = reuters.fileids()
-
-words9920 = reuters.words(['training/9920'])[:71]
-print(words9920)
